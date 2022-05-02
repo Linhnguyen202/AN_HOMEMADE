132df5472b8899f80ea3d766cade8caa56852aa9
Linhnguyen202 <ngglinh0914@gmail.com>
2022-05-02 16:16:35 +0700

    fix

--- src/Components/Admin/Form/Form.jsx
+++ src/Components/Admin/Form/Form.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import md5 from 'md5';
 const Form = () => {
+    const  [email,setEmail] = useState("")
+    const  [pass,setPass] = useState("")
+    
     return (
         <div className='flex items-center justify-center h-screen justif bg-gradient-to-r from-purple-500 to-pink-500'>
             <div  className = "w-[300px] p-3  bg-white rounded-lg shadow-2xl">
@@ -8,11 +11,15 @@ const Form = () => {
                     <h3 className='mb-4 text-2xl font-semibold'>Login</h3>
                     <div className='flex flex-col mb-4'>
                         <label htmlFor="" className='mb-1 text-sm'>Email</label>
-                        <InputItem placeholder="Enter your email..."></InputItem>
+                        <div className='bg-gray-200 rounded-md'>
+                            <input type="text"  onChange={(e)=>setEmail(e.target.value)} className="p-2 text-sm bg-transparent outline-none"/>
+                        </div>
                     </div>
                     <div className='flex flex-col mb-4'>
                         <label htmlFor="" className='mb-1 text-sm'>Password</label>
-                        <InputItem placeholder="Enter your password..."></InputItem>
+                        <div className='bg-gray-200 rounded-md'>
+                            <input type="text" onChange={(e)=>setPass(e.target.value)} className="p-2 text-sm bg-transparent outline-none"/>
+                        </div>
                     </div>
                     <div>
                         <button className='w-full py-2 text-white transition-opacity rounded-lg bg-secondColor hover:opacity-80'>LOGIN</button>
@@ -26,11 +33,3 @@ const Form = () => {
 
 export default Form;
 
-const InputItem = (props)=>{
-    return(
-        <div className='bg-gray-200 rounded-md'>
-             <input type="text" placeholder={props.placeholder} className="p-2 text-sm bg-transparent outline-none"/>
-        </div>
-       
-    )
-}
